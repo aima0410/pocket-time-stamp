3a7fe7fb167231b168ef7a637d7c1a2ceb694a89
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-08 11:47:27 +0900

    style :lipstick: ホームに未加工のパーツ設置

--- src/components/layouts/Home.tsx
+++ src/components/layouts/Home.tsx
@@ -1,3 +1,22 @@
+// ---- Next ----
+import Image from 'next/image';
+// ---- Images ----
+// ---- Components ----
+import Message from '@ui-parts/Message';
+import IdealTimeLine from '@ui-parts/IdealTimeLine';
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      Home
+      { <Message />}
+      <div>
+        {/* <Image src={} alt={} /> */}
+        <h3>ポケモン</h3>
+        <p>レベル30</p>
+        <div>グラフ</div>
+      </div>
+      <IdealTimeLine />
+    </>
+  );
 }
