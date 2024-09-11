a0f441fd2afe3a8a53a5232a2be4395220de212c
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-12 01:01:14 +0900

    feat :sparkles: 活動リストの初回時データをセット＋更新関数の改善

--- src/components/layouts/PocketTimeStamp.tsx
+++ src/components/layouts/PocketTimeStamp.tsx
@@ -3,10 +3,12 @@
 // ---- Next ----
 import { usePathname } from 'next/navigation';
 // ---- React ----
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // ---- Types ----
 import AppStatus from 'src/types/AppStatus';
 import Tab from 'src/types/Tab';
+// ---- Utils ----
+import getCurrentTab from 'src/utils/getCurrentTab';
 // ---- Components ----
 import TodayTimeLine from '@ui-parts/TodayTimeLine';
 import TabNav from '@layouts/TabNav';
@@ -15,38 +17,49 @@ import TimeStamp from '@layouts/TimeStamp';
 import Histories from '@layouts/Histories';
 import Reports from '@layouts/Reports';
 import Collection from './Collection';
+import { trackDynamicDataAccessed } from 'next/dist/server/app-render/dynamic-rendering';
 
+// ========== コンポーネント関数 ==========
 export default function PocketTimeStamp() {
-  // ---- アプリの操作ステータス ----
+  // -------- useState：宣言 --------
   const [appStatus, setAppStatus] = useState<AppStatus>('StandbyMode');
+  const [activities, setActivities] = useState<Array<string>>([]);
+
+  // -------- useState：stateの更新処理 --------
   const switchAppStatus = (newMode: AppStatus) => {
     setAppStatus(newMode);
   };
 
-  // ---- 現在選択中のタブ ----
-  const pathname = usePathname();
-  const currentTab: Tab = ((pathname: string) => {
-    switch (pathname) {
-      case '/':
-        return 'Home';
-      case '/time-stamp':
-        return 'CreateTimeStamp';
-      case '/histories':
-        return 'RecentHistories';
-      case '/reports':
-        return 'Reports';
-      case '/collection':
-        return 'Collection';
-      default:
-        return '404';
-    }
-  })(pathname);
-
-  // ---- アクティビティ ----
-  const [activities, setActivities] = useState<Array<string>>(['運動', '読書', '個人開発']);
   const updateActivities = (newActivitiesList: Array<string>) => {
-    setActivities(newActivitiesList);
+    // リスト内容に変更があるかどうかチェック
+    const beforeArr = [...activities].sort();
+    const afterArr = [...newActivitiesList].sort();
+    const isSameArr = beforeArr.every((beforeActive, i) => beforeActive === afterArr[i]);
+
+    if (!isSameArr) {
+      localStorage.setItem('activities', JSON.stringify(newActivitiesList));
+      setActivities(newActivitiesList);
+    }
   };
+
+  // -------- usePathname：現在選択しているタブを追跡 --------
+  const pathname = usePathname();
+  const currentTab: Tab = getCurrentTab(pathname);
+
+  // -------- useEffect：初回マウント時の処理 ---------
+  // アクティビティのデータセット
+  useEffect(() => {
+    const storedActivities = localStorage.getItem('activities');
+
+    if (storedActivities) {
+      const activities = JSON.parse(storedActivities);
+      setActivities(activities);
+    } else {
+      const activities = ['運動', '読書', '雑用'];
+      localStorage.setItem('activities', JSON.stringify(activities));
+      setActivities(activities);
+    }
+  }, []);
 
   return (
     <>
