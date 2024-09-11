658123859550717f6c79478477ef82446cc83dc0
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-11 20:48:58 +0900

    refactor :recycle: ログデータの型定義ファイルの作成

---
+++ src/types/LogData.d.ts
@@ -0,0 +1,10 @@
+type LogData = {
+  date: string | undefined;
+  dayOfWeek: number | undefined;
+  activity: string | undefined;
+  startTime: string | undefined;
+  endTime: string | undefined;
+  restTime: string | undefined;
+};
+
+export default LogData;
