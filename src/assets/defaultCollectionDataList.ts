e61cdc79b167465a0290ba71a250b3d510de6cee
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-13 20:45:41 +0900

    chore :building_construction: ポケモンコレクションの初期データ作成

---
+++ src/assets/defaultCollectionDataList.ts
@@ -0,0 +1,55 @@
+// ---- Types ----
+import CollectionData from 'src/types/CollectionData';
+
+const defaultCollectionDataList: Array<CollectionData> = [
+  {
+    // ---- ラルトス ----
+    selected: true,
+    level: 1,
+    XP: 0,
+    evolution: 0,
+    name: 'ralts',
+  },
+  {
+    // ---- ヒノアラシ ----
+    selected: false,
+    level: 1,
+    XP: 0,
+    evolution: 0,
+    name: 'cyndaquil',
+  },
+  {
+    // ---- ゴース ----
+    selected: false,
+    level: 1,
+    XP: 0,
+    evolution: 0,
+    name: 'gastly',
+  },
+  {
+    // ---- ココドラ ----
+    selected: false,
+    level: 1,
+    XP: 0,
+    evolution: 0,
+    name: 'aron',
+  },
+  {
+    // ---- ポッチャマ ----
+    selected: false,
+    level: 1,
+    XP: 0,
+    evolution: 0,
+    name: 'piplup',
+  },
+  {
+    // ---- フカマル ----
+    selected: false,
+    level: 1,
+    XP: 0,
+    evolution: 0,
+    name: 'gible',
+  },
+];
+
+export default defaultCollectionDataList;
