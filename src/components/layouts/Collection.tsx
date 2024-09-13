518ca1ecb09d14e459bd6b3af4a38c310c7ec4d3
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-14 00:14:11 +0900

    style :lipstick: コレクションページUX作成

--- src/components/layouts/Collection.tsx
+++ src/components/layouts/Collection.tsx
@@ -1,3 +1,63 @@
-export default function Collection() {
-  return <>Collection</>;
+// ---- Next ----
+import Image from 'next/image';
+// ---- React ----
+import { useEffect, useState } from 'react';
+// ---- Types ----
+import Pokemon from 'src/types/Pokemon';
+import DefaultPokemonName from 'src/types/PokemonName';
+import CollectionData from 'src/types/CollectionData';
+// ---- Utils ----
+import { sortPokemonCollection } from '@utils/sortUtils';
+
+// ========== 型定義 ==========
+interface Props {
+  pokemonList: Array<Pokemon>;
+  collectionDataList: Array<CollectionData>;
+  updateCollectionDataList: (latestList: Array<CollectionData>) => void;
+}
+
+// ========== コンポーネント関数 ==========
+export default function Collection({
+  pokemonList,
+  collectionDataList,
+  updateCollectionDataList,
+}: Props) {
+  // -------- useState：宣言 --------
+  const [selectedPokemon, setSelectedPokemon] = useState<DefaultPokemonName | string>('');
+
+  // -------- useState：state更新処理 --------
+  const trackSelectedPokemon = (targetPokemon: DefaultPokemonName | string) => {
+    const updatedCollectionOrder = collectionDataList.map((data) => ({
+      ...data,
+      selected: data.name === targetPokemon,
+    }));
+
+    updateCollectionDataList(updatedCollectionOrder);
+    setSelectedPokemon(targetPokemon);
+  };
+  // -------- useEffect：初回マウント時 --------
+  useEffect(() => {
+    if (collectionDataList.length > 0) {
+      trackSelectedPokemon(collectionDataList[0].name);
+    }
+  }, []);
+
+  return (
+    <section>
+      <ul>
+        {collectionDataList.map((pokemon) => (
+          <li
+            key={pokemon.name}
+            onClick={() => {
+              trackSelectedPokemon(pokemon.name);
+            }}
+          >
+            <Image src={pokemon.imageUrl} alt={pokemon.name} width={50} height={50} />
+            <h3>{pokemon.japaneseName}</h3>
+            <p>レベル{pokemon.level}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
