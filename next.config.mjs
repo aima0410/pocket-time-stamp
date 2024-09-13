c231ed2f507b91279501a67d1d438a1099d40163
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-13 12:41:15 +0900

    docs :memo: 画像最適化機能をOFF

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'dist'
+  distDir: 'dist',
+  images: {
+    unoptimized: true, // 画像最適化を無効化
+  },
 };
 
 export default nextConfig;
