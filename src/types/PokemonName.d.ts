b7d86dbea400bf72256b9590fbdf565a54eeccf1
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-13 12:43:16 +0900

    refactor :recycle: Pokemon名の型定義

---
+++ src/types/PokemonName.d.ts
@@ -0,0 +1,21 @@
+type PokemonName =
+  | 'ralts'
+  | 'kirlia'
+  | 'gardevoir'
+  | 'cyndaquil'
+  | 'quilava'
+  | 'typhlosion'
+  | 'gastly'
+  | 'haunter'
+  | 'gengar'
+  | 'aron'
+  | 'lairon'
+  | 'aggron'
+  | 'piplup'
+  | 'prinplup'
+  | 'empoleon'
+  | 'gible'
+  | 'gabite'
+  | 'garchomp';
+
+export default PokemonName;
