054d561b458bfeb630bb94f1f3a5644f8bb581ad
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-14 00:17:58 +0900

    feat :sparkles: 英語名の配列を元にデータ取得する機能

--- src/utils/fetchPokemonDataUtils.ts
+++ src/utils/fetchPokemonDataUtils.ts
@@ -1,7 +1,11 @@
 // ---- Types ----
-import PokemonName from "src/types/PokemonName";
+import CollectionData from 'src/types/CollectionData';
+import Pokemon from 'src/types/Pokemon';
+import DefaultPokemonName from 'src/types/PokemonName';
+import createDefaultCollectionDataList from './createDefaultCollectionDataList';
+import { sortPokemonCollection } from './sortUtils';
 
-export const fetchPokemonData = async (name: PokemonName) => {
+const fetchPokemonData = async (name: DefaultPokemonName | string) => {
   try {
     // ポケモンの種別情報を取得
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}/`);
@@ -18,7 +22,7 @@ export const fetchPokemonData = async (name: PokemonName) => {
     const evolutionChainData = await evolutionChainResponse.json();
 
     // 進化チェーンのすべての段階をリストとして取得
-    const chainNames: Array<string | null> = [];
+    const chainNames: Array<DefaultPokemonName | string | null> = [];
     let currentChain = evolutionChainData.chain;
     while (currentChain) {
       chainNames.push(currentChain.species.name);
@@ -35,4 +39,31 @@ export const fetchPokemonData = async (name: PokemonName) => {
     console.error('Failed to fetch Pokemon:', error);
     return null;
   }
+};
+
+export const fetchPokemonList = async (
+  pokemonNameList: Array<DefaultPokemonName | string>,
+  getPokemonList: (pokemonList: Array<Pokemon>) => void,
+  getCollectionDataList: (collectionList: Array<CollectionData>) => void,
+) => {
+  const pokemonDataList = await Promise.all(pokemonNameList.map((name) => fetchPokemonData(name)));
+
+  const filteredPokemonDataList = pokemonDataList.filter(
+    (pokemonData) =>
+      pokemonData !== null &&
+      pokemonData.evolutionChain.filter((name): name is string => name !== null),
+  ) as Array<Pokemon>;
+
+  getPokemonList(filteredPokemonDataList);
+  localStorage.setItem('pokemonList', JSON.stringify(filteredPokemonDataList));
+
+  const defaultCollection = createDefaultCollectionDataList(filteredPokemonDataList);
+
+  const filteredDefaultCollection = defaultCollection.filter(
+    (collection: CollectionData) => collection !== null,
+  );
+
+  const sortedDefaultCollection = sortPokemonCollection(filteredDefaultCollection);
+  getCollectionDataList(sortedDefaultCollection);
+  localStorage.setItem('collectionDataList', JSON.stringify(sortedDefaultCollection));
 };
