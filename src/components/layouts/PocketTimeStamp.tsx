ef43e35903445b9820520ee75f23de1e615612bd
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-14 00:24:48 +0900

    feat :sparkles: 初回マウント時のコレクションのデータセット機能

--- src/components/layouts/PocketTimeStamp.tsx
+++ src/components/layouts/PocketTimeStamp.tsx
@@ -7,8 +7,10 @@ import { useEffect, useState } from 'react';
 // ---- Types ----
 import AppStatus from 'src/types/AppStatus';
 import Tab from 'src/types/Tab';
+import CollectionData from 'src/types/CollectionData';
 // ---- Utils ----
 import getCurrentTab from 'src/utils/getCurrentTab';
+import { fetchPokemonList } from '@utils/fetchPokemonDataUtils';
 // ---- Components ----
 import TodayTimeLine from '@ui-parts/TodayTimeLine';
 import TabNav from '@layouts/TabNav';
@@ -17,12 +19,18 @@ import TimeStamp from '@layouts/TimeStamp';
 import Histories from '@layouts/Histories';
 import Reports from '@layouts/Reports';
 import Collection from './Collection';
+// ---- Constant ----
+import defaultPokemonNameList from '@assets/pokemonNamesList';
+import Pokemon from 'src/types/Pokemon';
+import { sortPokemonCollection } from '@utils/sortUtils';
 
 // ========== コンポーネント関数 ==========
 export default function PocketTimeStamp() {
   // -------- useState：宣言 --------
   const [appStatus, setAppStatus] = useState<AppStatus>('StandbyMode');
   const [activities, setActivities] = useState<Array<string>>([]);
+  const [pokemonList, setPokemonList] = useState<Array<Pokemon>>([]);
+  const [collectionDataList, setCollectionDataList] = useState<Array<CollectionData>>([]);
 
   // -------- useState：stateの更新処理 --------
   const switchAppStatus = (newMode: AppStatus) => {
@@ -40,23 +48,52 @@ export default function PocketTimeStamp() {
       setActivities(newActivitiesList);
     }
   };
+  const getPokemonList = (pokemonList: Array<Pokemon>) => {
+    setPokemonList(pokemonList);
+  };
+
+  // あとでuseReducerでまとめる。getとupdateで。
+  const getCollectionDataList = (collectionDataList: Array<CollectionData>) => {
+    setCollectionDataList(collectionDataList);
+  };
+
+  const updateCollectionDataList = (collectionDataList: Array<CollectionData>) => {
+    const sortedCollectionDataList = sortPokemonCollection(collectionDataList);
+
+    localStorage.setItem('collectionDataList', JSON.stringify(sortedCollectionDataList));
+    setCollectionDataList(sortedCollectionDataList);
+  };
 
   // -------- usePathname：現在選択しているタブを追跡 --------
   const pathname = usePathname();
   const currentTab: Tab = getCurrentTab(pathname);
 
   // -------- useEffect：初回マウント時の処理 ---------
-  // アクティビティのデータセット
   useEffect(() => {
+    // ---- アクティビティのデータセット ----
     const storedActivities = localStorage.getItem('activities');
-
     if (storedActivities) {
       const activities = JSON.parse(storedActivities);
       setActivities(activities);
     } else {
+      // 初期のデモデータをセット
       const activities = ['運動', '読書', '雑用'];
       localStorage.setItem('activities', JSON.stringify(activities));
       setActivities(activities);
+    }
+
+    // ---- ポケモンのデータセット ----
+    const storedPokemonList = localStorage.getItem('pokemonList');
+    const storedCollectionDataList = localStorage.getItem('collectionDataList');
+    if (storedPokemonList && storedCollectionDataList) {
+      // ある：ローカルストレージのデータをセット
+      getPokemonList(JSON.parse(storedPokemonList));
+      getCollectionDataList(JSON.parse(storedCollectionDataList));
+      JSON.parse(storedCollectionDataList).length === 0 &&
+        fetchPokemonList(defaultPokemonNameList, getPokemonList, getCollectionDataList);
+    } else {
+      // なし：PokemonAPIからデータを取得してセット
+      fetchPokemonList(defaultPokemonNameList, getPokemonList, getCollectionDataList);
     }
   }, []);
 
@@ -90,8 +127,43 @@ export default function PocketTimeStamp() {
           />
         )}
         {currentTab === 'Reports' && <Reports />}
-        {currentTab === 'Collection' && <Collection />}
+        {currentTab === 'Collection' && (
+          <Collection
+            pokemonList={pokemonList}
+            collectionDataList={collectionDataList}
+            updateCollectionDataList={updateCollectionDataList}
+          />
+        )}
       </section>
     </>
   );
 }
+
+// ---- ポケモンIDのメモ ----
+// -- ピックアップ --
+// ラルトス280、281、282
+// ヒノアラシ155、156、157
+// ゴース92、93、94
+// ココドラ304、305、306
+// ポッチャマ393、394、395
+// フカマル443、444、445445
+
+// -- 別候補 --
+// ホゲータ909、910、911
+// ヒトモシ607、608、609
+// フシギダネ1、2，3
+// ヒトカゲ４，５，６
+// ゼニガメ７、８，９
+// イシツブテ74、75、76
+// ユニラン577、578、579
+// キバゴ610、611、612
+// フォッコ653、654、655
+// ケロマツ656、657、658
+// ヤヤマコ661、662、663
+// コフキムシ664、665、666
+// ジャラコ782、783、784
+// パも921、922、923
+// メッソン816、817、818
+// ココガラ821、822、823
+// アマカジ761、762、763
+// ヌメラ704、705、706
