0dc5430c15cd2245721497c01aec5c42a01c4265
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-13 12:42:39 +0900

    refactor :recycle: 取得したいPokemon名の配列リスト

---
+++ src/assets/pokemonNamesList.ts
@@ -0,0 +1,26 @@
+// ---- Types ----
+import PokemonName from 'src/types/PokemonName';
+
+// 取得したいポケモンの名前リスト
+const pokemonNamesList: Array<PokemonName> = [
+  'ralts',
+  'kirlia',
+  'gardevoir',
+  'cyndaquil',
+  'quilava',
+  'typhlosion',
+  'gastly',
+  'haunter',
+  'gengar',
+  'aron',
+  'lairon',
+  'aggron',
+  'piplup',
+  'prinplup',
+  'empoleon',
+  'gible',
+  'gabite',
+  'garchomp',
+];
+
+export default pokemonNamesList;
