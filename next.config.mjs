559c88e11c042d4f5d7240e7e297453f0d5c4398
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-15 06:12:21 +0900

    dep :package: KumaUIの設定

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,6 @@ const nextConfig = {
 };
 
 export default withKumaUI(nextConfig, {
-  // The destination to emit an actual CSS file. If not provided, the CSS will be injected via virtual modules.
-  outputDir: "./.kuma", // Optional
   // Enable WebAssembly support for Kuma UI. Default is false and will use Babel to transpile the code.
   wasm: true // Optional
 });
