f1ba47202c7b95d69b2befc418ebbb1458594ce5
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-20 04:03:46 +0900

    delete :wastebasket: 曜日、休息タイムのデータ項目を削除

--- src/types/LogData.d.ts
+++ src/types/LogData.d.ts
@@ -1,10 +1,8 @@
-type LogData = {
-  date: string;
-  dayOfWeek: number;
-  activity: string | null;
-  startTime: string;
-  endTime: string;
-  restTime: number;
-};
+interface LogData {
+  date: string,
+  activity: string,
+  startTime: string,
+  endTime: string,
+}
 
 export default LogData;
