e618f581b66cb9183eef68c00b86f06a19c48f2f
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-20 03:56:37 +0900

    docs :memo: レベルアップに必要な経験値＆累計の経験値の表

---
+++ src/assets/LevelTable.ts
@@ -0,0 +1,37 @@
+// ---- Types ----
+import ExperienceTable from "src/types/Level";
+
+const levelTable: ExperienceTable = [
+  { level: 1, requiredExp: 0, totalExp: 0 },
+  { level: 2, requiredExp: 200, totalExp: 200 },
+  { level: 3, requiredExp: 450, totalExp: 650 },
+  { level: 4, requiredExp: 750, totalExp: 1400 },
+  { level: 5, requiredExp: 1100, totalExp: 2500 },
+  { level: 6, requiredExp: 1500, totalExp: 4000 },
+  { level: 7, requiredExp: 1950, totalExp: 5950 },
+  { level: 8, requiredExp: 2450, totalExp: 8400 },
+  { level: 9, requiredExp: 3000, totalExp: 11400 },
+  { level: 10, requiredExp: 3600, totalExp: 15000 },
+  { level: 11, requiredExp: 4250, totalExp: 19250 },
+  { level: 12, requiredExp: 4950, totalExp: 24200 },
+  { level: 13, requiredExp: 5700, totalExp: 29900 },
+  { level: 14, requiredExp: 6500, totalExp: 36400 },
+  { level: 15, requiredExp: 7350, totalExp: 43750 },
+  { level: 16, requiredExp: 8250, totalExp: 52000 },
+  { level: 17, requiredExp: 9200, totalExp: 61200 },
+  { level: 18, requiredExp: 10200, totalExp: 71400 },
+  { level: 19, requiredExp: 11250, totalExp: 82650 },
+  { level: 20, requiredExp: 12350, totalExp: 95000 },
+  { level: 21, requiredExp: 13500, totalExp: 108500 },
+  { level: 22, requiredExp: 14700, totalExp: 123200 },
+  { level: 23, requiredExp: 15950, totalExp: 139150 },
+  { level: 24, requiredExp: 17250, totalExp: 156400 },
+  { level: 25, requiredExp: 18600, totalExp: 175000 },
+  { level: 26, requiredExp: 20000, totalExp: 195000 },
+  { level: 27, requiredExp: 21450, totalExp: 216450 },
+  { level: 28, requiredExp: 22950, totalExp: 239400 },
+  { level: 29, requiredExp: 24500, totalExp: 263900 },
+  { level: 30, requiredExp: 26100, totalExp: 290000 },
+];
+
+export default levelTable;
