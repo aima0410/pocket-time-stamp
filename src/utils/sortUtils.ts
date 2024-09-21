a5c9844f406f72a70f53e2cd07fd0e32c0716e57
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-21 13:46:47 +0900

    chore :building_construction: insertLog関数を別ファイルへ、returnを新規配列で

--- src/utils/sortUtils.ts
+++ src/utils/sortUtils.ts
@@ -1,63 +1,8 @@
 // ---- Types ----
-import LogData from 'src/types/LogData';
 import CollectionData from 'src/types/CollectionData';
 import { DailyData, Line } from 'src/types/ReportsData';
 
-interface insertLogArgument {
-  newLog: LogData;
-  existingLogs: Array<LogData>;
-}
-
-export function insertLog({ newLog, existingLogs }: insertLogArgument) {
-  const newStart = new Date(`${newLog.date} ${newLog.startTime}`);
-  const newEnd = new Date(`${newLog.date} ${newLog.endTime}`);
-
-  let updatedLogs = [];
-
-  for (const log of existingLogs) {
-    const existingStart = new Date(`${log.date} ${log.startTime}`);
-    const existingEnd = new Date(`${log.date} ${log.endTime}`);
-
-    if (log.date !== newLog.date) {
-      // 日付が異なるログはそのまま追加
-      updatedLogs.push(log);
-    } else {
-      // 日付が同じ場合、重なりを処理する
-      if (newEnd <= existingStart || newStart >= existingEnd) {
-        // 重なっていない場合
-        updatedLogs.push(log);
-      } else if (newStart <= existingStart && newEnd >= existingEnd) {
-        // 新規ログが既存ログを完全に包含する場合
-        // 既存ログを破棄する（何もしない）
-      } else if (newStart > existingStart && newEnd < existingEnd) {
-        // 新規ログが既存ログの中に収まる場合
-        // 既存ログを前半と後半に分割して追加
-        updatedLogs.push({ ...log, endTime: newLog.startTime });
-        updatedLogs.push({ ...log, startTime: newLog.endTime });
-      } else if (newStart > existingStart && newStart < existingEnd) {
-        // 新規ログの開始時刻が既存ログの途中にあり、終了時刻が既存ログの終了時刻を超えている場合
-        updatedLogs.push({ ...log, endTime: newLog.startTime });
-      } else if (newEnd > existingStart && newEnd < existingEnd) {
-        // 新規ログの開始時刻が既存ログの開始時刻より前で、終了時刻が既存ログの途中にある場合
-        updatedLogs.push({ ...log, startTime: newLog.endTime });
-      }
-    }
-  }
-
-  // 新規ログを追加する
-  updatedLogs.push(newLog);
-
-  // 日付と開始時刻でログをソートする
-  updatedLogs.sort((a, b) => {
-    if (a.date !== b.date) {
-      return a.date.localeCompare(b.date);
-    } else {
-      return a.startTime.localeCompare(b.startTime);
-    }
-  });
-
-  return updatedLogs;
-}
+// ========================================
 
 export function sortPokemonCollection(pokemonArray: Array<CollectionData>): Array<CollectionData> {
   return pokemonArray.sort((a, b) => {
@@ -73,8 +18,9 @@ export function sortPokemonCollection(pokemonArray: Array<CollectionData>): Arra
   });
 }
 
+// ========================================
 export function sortTimelineDescending(timeline: Array<Line>): Array<Line> {
-  return timeline.sort((a, b) => {
+  return [...timeline].sort((a, b) => {
     // "00:00"形式の文字列を分と秒に分割
     const [aHours, aMinutes] = a.startTime.split(':').map(Number);
     const [bHours, bMinutes] = b.startTime.split(':').map(Number);
@@ -88,6 +34,7 @@ export function sortTimelineDescending(timeline: Array<Line>): Array<Line> {
   });
 }
 
+// ========================================
 /**
  * DailyDataの配列を日付順にソートする関数
  * @param dailyDataArray ソートするDailyDataの配列
