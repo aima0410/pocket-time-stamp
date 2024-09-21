b25ceb546cd24e1dcd0afc7b1b18368610a227c6
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-21 17:32:25 +0900

    chore :building_construction: 隣接ログ統合機能追加、定数名の変更

--- src/utils/insertLog.ts
+++ src/utils/insertLog.ts
@@ -2,7 +2,8 @@
 import LogData from 'src/types/LogData';
 import { DailyData, Line } from 'src/types/ReportsData';
 // ---- Utils ----
-import { sortDailyDataByDate, sortTimelineDescending } from './sortUtils';
+import { sortDailyDataByDate, sortTimelineDescending } from '@utils/sortUtils';
+import { mergeAdjacentLogs } from '@utils/timeLineUtils';
 
 interface InsertLogArgument {
   unconfirmedNewLog: LogData;
@@ -19,12 +20,15 @@ export default function insertLog({
   unconfirmedNewLog,
   existingDailyData,
 }: InsertLogArgument): Array<DailyData> {
+  // 深い階層まで既存データをまとめてコピー
   const updatedLogs: Array<DailyData> = JSON.parse(JSON.stringify(existingDailyData));
 
+  // 上書きしたいログと同じ日付を持つ既存ログを検索
   const targetDateIndex = updatedLogs.findIndex(
     (dayData) => dayData.date === unconfirmedNewLog.date,
   );
 
+  // どの既存ログとも日付が被らなかった場合はそのまま追加
   if (targetDateIndex === -1) {
     updatedLogs.push({
       date: unconfirmedNewLog.date,
@@ -37,29 +41,39 @@ export default function insertLog({
       ],
     });
   } else {
+    // 日付が同じ既存ログのタイムラインを取得
     const targetTimeLine = updatedLogs[targetDateIndex].timeLine;
+
+    // エントリータイムラインの作成
     const newTimeLine: Array<Line> = [];
 
-    const editedStart = new Date(`1970-01-01T${unconfirmedNewLog.startTime}`);
-    const editedEnd = new Date(`1970-01-01T${unconfirmedNewLog.endTime}`);
+    // 上書きしたいログのDateを取得
+    const unconfirmedStart = new Date(`1970-01-01T${unconfirmedNewLog.startTime}`);
+    const unconfirmedEnd = new Date(
+      `1970-01-01T${unconfirmedNewLog.endTime === '00:00' ? '24:00' : unconfirmedNewLog.endTime}`,
+    );
 
+    // タイムラインから既存ログを一つずつ取り出してエントリーデータに追加
     for (const existingLog of targetTimeLine) {
+      // 既存ログのDateを取得
       const existingStart = new Date(`1970-01-01T${existingLog.startTime}`);
-      const existingEnd = new Date(`1970-01-01T${existingLog.endTime}`);
+      const existingEnd = new Date(
+        `1970-01-01T${existingLog.endTime === '00:00' ? '24:00' : existingLog.endTime}`,
+      );
 
-      if (editedEnd <= existingStart || editedStart >= existingEnd) {
+      if (unconfirmedEnd <= existingStart || unconfirmedStart >= existingEnd) {
         // 重複しない場合は既存のログをそのまま追加
         newTimeLine.push(existingLog);
       } else {
         // 重複する場合、重複しない部分を追加
-        if (existingStart < editedStart) {
+        if (existingStart < unconfirmedStart) {
           newTimeLine.push({
             startTime: existingLog.startTime,
             endTime: unconfirmedNewLog.startTime,
             activity: existingLog.activity,
           });
         }
-        if (existingEnd > editedEnd) {
+        if (existingEnd > unconfirmedEnd) {
           newTimeLine.push({
             startTime: unconfirmedNewLog.endTime,
             endTime: existingLog.endTime,
@@ -79,11 +93,14 @@ export default function insertLog({
     // 更新されたタイムラインを開始時刻でソート
     const sortedTimeLine = sortTimelineDescending(newTimeLine);
 
-    // 更新されたタイムラインで既存のデータを置き換え
-    updatedLogs[targetDateIndex].timeLine = sortedTimeLine;
+    // 隣接するログをマージ
+    const mergedTimeLine = mergeAdjacentLogs(sortedTimeLine);
+
+    // 作成したタイムラインでエントリーデータを更新
+    updatedLogs[targetDateIndex].timeLine = mergedTimeLine;
   }
 
-  // 全体のログを日付でソート（最新の日付が先頭に来るように）
+  // エントリーデータをまとめで日付でソート（最新の日付が先頭に来るように）
   const insertedDailyData = sortDailyDataByDate(updatedLogs);
 
   return insertedDailyData;
