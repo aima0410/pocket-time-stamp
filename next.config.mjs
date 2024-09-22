fd0ccd870d85a92078de25ce1d05011a6889ec98
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-23 00:16:20 +0900

    docs :memo: Vercelでの公開用に修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
-// ---- KumaUI ----
 import { withKumaUI } from "@kuma-ui/next-plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true, // 画像最適化を無効化
-  },
 };
 
 export default withKumaUI(nextConfig, {
-  // Enable WebAssembly support for Kuma UI. Default is false and will use Babel to transpile the code.
   wasm: true // Optional
 });
