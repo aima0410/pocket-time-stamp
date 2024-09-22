fb4530d3bd8f4218c82cf285f09460d681e728fd
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-22 21:15:05 +0900

    refactor :recycle: ミリ秒算出する関数を別ファイルに切り出し

--- src/utils/calculate.ts
+++ src/utils/calculate.ts
@@ -3,3 +3,16 @@ export function getRandomInt(min: number, max: number) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function getMilliseconds(startTime: string, endTime: string) {
+  // -- startTime --
+  const startTimeForCalc = new Date(`2000/01/01 ${startTime}`).getTime();
+
+  // -- endTime --
+  let endDate = new Date(`2000/01/01 ${endTime}`);
+  endTime === '00:00' && endDate.setDate(endDate.getDate() + 1);
+  const endTimeForCalc = endDate.getTime();
+
+  const milliseconds = endTimeForCalc - startTimeForCalc;
+  return milliseconds;
+}
--- src/utils/calculateTimeUtils.ts
+++ src/utils/calculateTimeUtils.ts
@@ -1,18 +1,11 @@
 // ---- Types ----
 import { Line } from 'src/types/ReportsData';
+import { getMilliseconds } from '@utils/calculate';
 
-export function calculateWorkingTime(date: string, log: Line) {
-  // -- startTime --
-  const startTime = new Date(`${date} ${log.startTime}`).getTime();
-
-  // -- endTime --
-  let endDate = new Date(`${date} ${log.endTime}`);
-  log.endTime === '00:00' && endDate.setDate(endDate.getDate() + 1);
-  const endTime = endDate.getTime();
+export function calculateWorkingTime(log: Line) {
+  const milliseconds = getMilliseconds(log.startTime, log.endTime);
 
   // -- WorkingTime --
-  const subtractionResult = endTime - startTime;
-  const milliseconds = subtractionResult;
   const hours = Math.floor(milliseconds / (1000 * 60 * 60));
   const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
   return `${hours}時間 ${minutes}分`;
