6b80323400177e8b23f9c048d3ada70254bfb2bf
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-23 00:26:22 +0900

    fix :bug: 外部のドメインから画像を読み込む許可

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import { withKumaUI } from "@kuma-ui/next-plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // output: 'export',
+  // distDir: 'dist',
+  images: {
+    domains: ['raw.githubusercontent.com'],
+    // unoptimized: true, // 画像最適化を無効化
+  },
 };
 
 export default withKumaUI(nextConfig, {
