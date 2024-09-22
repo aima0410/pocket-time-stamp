a9c8a779e2652c3fd337b663591b697a3da7e8b4
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-22 21:17:53 +0900

    feat :sparkles: 月次データと累計データの再計算

--- src/components/layouts/Reports.tsx
+++ src/components/layouts/Reports.tsx
@@ -1,22 +1,38 @@
+// ---- Next ----
+import Image from 'next/image';
 // ---- React ----
 import { useState, useEffect } from 'react';
 // ---- Types ----
 import ReportTab from 'src/types/ReportTab';
 import { DailyData, MonthlyData, TotalData } from 'src/types/ReportsData';
+// ---- Images ----
+import reloadIcon from '@assets/images/reload.svg';
 // ---- Components ----
-import DayReport from '@ui-elements/DayReports';
-import MonthReport from '@ui-elements/MonthReports';
+import DailyReport from '@ui-elements/DailyReports';
+import MonthlyReport from '@ui-elements/MonthlyReports';
 import TotalReport from '@ui-elements/TotalReport';
+import { createMonthlyData, createTotalData } from '@utils/createReportDataUtils';
+import LogData from 'src/types/LogData';
 
 // =========== 型定義 ==========
 interface Props {
+  isDemo: boolean;
   dailyData: Array<DailyData>;
   monthlyData: Array<MonthlyData>;
+  updateMonthlyData: (newMonthlyData: Array<MonthlyData>) => void;
   totalData: Array<TotalData>;
+  updateTotalData: (newTotalData: Array<TotalData>) => void;
 }
 
 // =========== コンポーネント関数 ==========
-export default function Reports({ dailyData, monthlyData, totalData }: Props) {
+export default function Reports({
+  isDemo,
+  dailyData,
+  monthlyData,
+  updateMonthlyData,
+  totalData,
+  updateTotalData,
+}: Props) {
   // -------- useState：宣言 --------
   const [selectedTab, setSelectedTab] = useState<ReportTab>('DayTab');
 
@@ -25,12 +41,34 @@ export default function Reports({ dailyData, monthlyData, totalData }: Props) {
     setSelectedTab(nextTab);
   };
 
+  // -------- イベントハンドラ --------
+  const handleRecalculation = () => {
+    let selectedDailyData: Array<DailyData> = [];
+
+    const storedDailyData = localStorage.getItem('dailyData');
+    if (storedDailyData && !isDemo) {
+      selectedDailyData = JSON.parse(storedDailyData);
+    } else {
+      selectedDailyData = dailyData;
+    }
+
+    const recalcMonthlyData: Array<MonthlyData> = createMonthlyData(selectedDailyData);
+    const recalcTotalData: Array<TotalData> = createTotalData(recalcMonthlyData);
+
+    updateMonthlyData(recalcMonthlyData);
+    updateTotalData(recalcTotalData);
+  };
+
   // --------- useEffect：初回マウント時 --------
   useEffect(() => {}, []);
 
+  // -------- JSX --------
   return (
     <>
       <section>
+        <button onClick={handleRecalculation}>
+          <Image src={reloadIcon} alt="レポート内容をリロードするボタン" width={17} height={17} />
+        </button>
         <ul>
           <li>
             <button onClick={() => trackSelectedTab('DayTab')} disabled={selectedTab === 'DayTab'}>
@@ -54,9 +92,9 @@ export default function Reports({ dailyData, monthlyData, totalData }: Props) {
             </button>
           </li>
         </ul>
-        {selectedTab === 'DayTab' && <DayReport />}
-        {selectedTab === 'MonthTab' && <MonthReport />}
-        {selectedTab === 'TotalTab' && <TotalReport />}
+        {selectedTab === 'DayTab' && <DailyReport dailyData={dailyData} />}
+        {selectedTab === 'MonthTab' && <MonthlyReport monthlyData={monthlyData} />}
+        {selectedTab === 'TotalTab' && <TotalReport totalData={totalData} />}
       </section>
     </>
   );
