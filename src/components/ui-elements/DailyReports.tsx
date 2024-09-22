a9d515d15de0995e33527d22a1c44c62e9a17d3a
かりこ <webcreate0303yokoi0630@gmail.com>
2024-09-22 22:38:05 +0900

    feat :sparkles: 一度に表示する範囲を限定、prevもしくはnextボタンで表示範囲を変更

--- src/components/ui-elements/DailyReports.tsx
+++ src/components/ui-elements/DailyReports.tsx
@@ -1,5 +1,11 @@
+// ---- Next ----
+import Image from 'next/image';
+// ---- React ----
+import { useState } from 'react';
 // ---- Types ----
 import { DailyData } from 'src/types/ReportsData';
+// ---- Images ----
+import arrowIcon from '@assets/images/arrow.svg';
 // ---- Components ----
 import IdealTimeLine from '@ui-parts/IdealTimeLine';
 
@@ -11,17 +17,33 @@ interface Props {
 // ========== コンポーネント関数 ==========
 
 export default function DailyReport({ dailyData }: Props) {
+  // -------- useState：宣言 --------
+  const [visibleItems, setVisibleItems] = useState<number>(0);
+
+  // -------- イベントハンドラ --------
+  const handleLoadPrev = () => {
+    setVisibleItems((visibleItems) => visibleItems - 1);
+  };
+
+  const handleLoadNext = () => {
+    setVisibleItems((visibleItems) => visibleItems + 1);
+  };
+
+  // -------- JSX --------
   return (
     <>
       <div>
         <IdealTimeLine />
+        <button onClick={handleLoadPrev} disabled={visibleItems === 0}>
+          <Image src={arrowIcon} alt="前のログを表示" width={14} height={14} />
+        </button>
         <ol>
-          {dailyData.map((data) => (
-            <li>
+          {dailyData.slice(visibleItems, visibleItems + 5).map((data) => (
+            <li key={data.date}>
               <h3>{data.date}</h3>
               <div>
                 {data.timeLine.map((log) => (
-                  <span>
+                  <span key={`${data.date}-${log.startTime}`}>
                     開始時刻{log.startTime} / 活動内容{log.activity} / 終了時刻{log.endTime}
                   </span>
                 ))}
@@ -29,6 +51,9 @@ export default function DailyReport({ dailyData }: Props) {
             </li>
           ))}
         </ol>
+        <button onClick={handleLoadNext} disabled={visibleItems === dailyData.length - 5}>
+          <Image src={arrowIcon} alt="次のログを表示" width={14} height={14} />
+        </button>
       </div>
     </>
   );
--- src/components/ui-elements/MonthlyReports.tsx
+++ src/components/ui-elements/MonthlyReports.tsx
@@ -1,5 +1,11 @@
+// ---- Next ----
+import Image from 'next/image';
+// ---- React ----
+import { useState } from 'react';
 // ---- Types ----
 import { MonthlyData } from 'src/types/ReportsData';
+// ---- Images ----
+import arrowIcon from '@assets/images/arrow.svg';
 
 // ========== 型定義 ==========
 interface Props {
@@ -8,15 +14,31 @@ interface Props {
 
 // ========== コンポーネント関数 ==========
 export default function MonthlyReport({ monthlyData }: Props) {
+  // -------- useState：宣言 --------
+  const [visibleItems, setVisibleItems] = useState<number>(0);
+
+  // -------- イベントハンドラ --------
+  const handleLoadPrev = () => {
+    setVisibleItems((visibleItems) => visibleItems - 1);
+  };
+
+  const handleLoadNext = () => {
+    setVisibleItems((visibleItems) => visibleItems + 1);
+  };
+
+  // -------- JSX --------
   return (
     <>
+      <button onClick={handleLoadPrev} disabled={visibleItems === 0}>
+        <Image src={arrowIcon} alt="前のログを表示" width={14} height={14} />
+      </button>
       <ol>
-        {monthlyData.map((data) => (
-          <li>
+        {monthlyData.slice(visibleItems, visibleItems + 3).map((data) => (
+          <li key={data.date}>
             <h3>{data.date}</h3>
             <ol>
               {data.recordedTime.map((log) => (
-                <li>
+                <li key={`${data.date}-${log.activity}`}>
                   {log.activity} <span>{log.totalTime}</span>
                 </li>
               ))}
@@ -24,6 +46,9 @@ export default function MonthlyReport({ monthlyData }: Props) {
           </li>
         ))}
       </ol>
+      <button onClick={handleLoadNext} disabled={visibleItems === monthlyData.length - 3}>
+        <Image src={arrowIcon} alt="次のログを表示" width={14} height={14} />
+      </button>
     </>
   );
 }
